f612752366cbf70f2a762a164bec335ed636a9c6
Jens Staahl <stahl.jens@gmail.com>
2014-10-28 23:23:02 +0100

    woho explore + monsters working

--- main.hs
+++ main.hs
@@ -1,7 +1,8 @@
 import Graphics.UI.SDL as SDL
 import Graphics.UI.SDL.TTF as TTF
 import Graphics.UI.SDL.Image as SDLi
--- import Debug.Trace
+import Data.List
+import Debug.Trace
 
 -- import System.Random
 
@@ -17,7 +18,9 @@ data GameState = GameState {
   image :: Surface,
   running :: Bool,
   clickpos :: [Coord],
-  board :: [(Int, Int, Int)]
+  fov :: [(Int, Int, Int)],
+  pf :: [(Int, Int, Int)],
+  enemies :: [(Int, Int, Int)]
 }
 
 main :: IO()
@@ -33,9 +36,9 @@ main = do
   char <- SDLi.load "image/rpg_sprites_10.PNG"
 
   let b = [(x, y, if x == 1 && y ==1 then 1 else 0) | x <- [0..width-1], y <- [0..height-1]]
-  putStrLn $ show b
+  let pf' = [(x,y,z) | x <- [0..width-1], y <- [0..height-1], z <- [0,1], z == (x+y) `mod` 2]
 
-  gameLoop $ GameState char True [(10, 10)] b
+  gameLoop $ GameState char True [(10, 10)] b pf' [(0,0,1)]
 
 gameLoop :: GameState -> IO ()
 gameLoop gs = do
@@ -48,8 +51,11 @@ gameLoop gs = do
   let blit = (\(a, b) -> blitSurface (image gs) (Just $ Rect 70 70 32 32) s (Just (Rect a b 0 0)))
   mapM_ blit (clickpos gs)
 
-  let fillVal = (\(x, y, val) -> fillRect s (Just $ Rect (x*32) (y*32) 32 32) (Pixel (255*(fromIntegral val))))
-  mapM_ fillVal (board gs)
+  fillRect s (Just $ Rect 0 0 800 608) (Pixel 0)
+  
+  let explored_pf = filter (\(x, y, _) -> (x,y,1) `elem` (fov gs)) (pf gs)
+  let fillVal = (\(x, y, val) -> fillRect s (Just $ Rect (x*32) (y*32) 32 32) (Pixel (100*(1+(fromIntegral val)))))
+  mapM_ fillVal explored_pf
 
   case running gs of
     True -> gameLoop gs'
@@ -63,11 +69,20 @@ tickGame gs = do
 
 --Explore map
 handleClick :: GameState -> Coord -> GameState
-handleClick gs (mx, my) =  gs {board = board'}
-  where board' = map (u (mx, my)) (board gs)
-        (_,_,zz) = head $ filter (\(x',y',_) -> x' == mx && y' == my) (board gs)
+handleClick gs (mx, my)
+  | notFree = trace "NOT" gs -- not explored yet, nothing happens
+  | hasEnemy = trace "Enemy" gs
+  | otherwise = gs {fov = fov'}
+  where notFree = (mx, my, 0) `elem` (fov gs)
+        hasEnemy = length (filter (\(x,y,_) -> x == mx && y == my) (enemies gs)) > 0
+        fov' = map (u (mx, my)) (fov gs)
+        (_,_,zz) = head $ filter (\(x',y',_) -> x' == mx && y' == my) (fov gs)
+        (_,_,pfv) = head $ filter (\(x,y,_) -> x == mx && y == my) (pf gs)
         u = (\(x, y) (x', y', val) -> 
-          if zz == 1 && abs (x' - x) <= 1 && abs (y' - y) <= 1
+          if zz == 1 && 
+              abs (x' - x) <= 1 && 
+              abs (y' - y) <= 1 &&
+              pfv == 0
             then (x', y', 1) 
             else (x', y', val))
 
