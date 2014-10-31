167d7ea1fba0021683fc80ce63b28db4345710fc
Jens Staahl <stahl.jens@gmail.com>
2014-10-31 12:19:48 +0100

    Show player/enemy HP and sprites

---
+++ Model.hs
@@ -0,0 +1,45 @@
+module Model where
+
+import Graphics.UI.SDL as SDL
+import Graphics.UI.SDL.TTF as TTF
+
+height :: Int
+height = 19
+
+width :: Int
+width = 19
+
+type Coord = (Int, Int)
+
+data Player = Player {
+  pPos :: Coord,
+  pHealth :: Int,
+  pMaxHealth :: Int
+}
+
+data Enemy = Enemy {
+  ePos :: Coord,
+  eHealth :: Int,
+  eMaxHealth :: Int
+}
+
+data Graphics = Graphics {
+  test :: Surface,
+  playerSurface :: Surface,
+  enemySurface :: Surface,
+  floorSurface :: Surface,
+  wallSurface :: Surface
+}
+
+data GameState = GameState {
+  graphics :: Graphics,
+  running :: Bool,
+  clickpos :: [Coord],
+  fov :: [(Int, Int, Int)],
+  pf :: [(Int, Int, Int)],
+  enemies :: [Enemy],
+  gPlayer :: Player,
+  font :: Font,
+  gEnemyMouse :: Int
+}
+
--- main.hs
+++ main.hs
@@ -1,27 +1,10 @@
 import Graphics.UI.SDL as SDL
 import Graphics.UI.SDL.TTF as TTF
 import Graphics.UI.SDL.Image as SDLi
-import Data.List
+
 import Debug.Trace
 
--- import System.Random
-
-height :: Int
-height = 19
-
-width :: Int
-width = 19
-
-type Coord = (Int, Int)
-
-data GameState = GameState {
-  image :: Surface,
-  running :: Bool,
-  clickpos :: [Coord],
-  fov :: [(Int, Int, Int)],
-  pf :: [(Int, Int, Int)],
-  enemies :: [(Int, Int, Int)]
-}
+import Model
 
 main :: IO()
 main = do
@@ -33,29 +16,57 @@ main = do
 
   enableKeyRepeat 500 30
 
+  fnt <- openFont "font.ttf" 30
   char <- SDLi.load "image/rpg_sprites_10.PNG"
+  mainchar <- SDLi.load "image/char.png"
+  mob <- SDLi.load "image/enemy.png"
+  floor_ <- SDLi.load "image/floor.png"
+  wall <- SDLi.load "image/wall.png"
 
-  let b = [(x, y, if x == 1 && y ==1 then 1 else 0) | x <- [0..width-1], y <- [0..height-1]]
+  let fov' = [(x, y, if x <= 2 && y <= 2 then 1 else 0) | x <- [0..width-1], y <- [0..height-1]]
   let pf' = [(x,y,z) | x <- [0..width-1], y <- [0..height-1], z <- [0,1], z == (x+y) `mod` 2]
 
-  gameLoop $ GameState char True [(10, 10)] b pf' [(0,0,1)]
+  let gs = Graphics char mainchar mob floor_ wall
+  let p = Player (1,1) 10 10
+  gameLoop $ GameState gs True [(10, 10)] fov' pf' [Enemy (0,0) 10 10] p fnt (0)
+
+drawSprite :: Surface -> Surface -> Coord -> IO(Bool)
+drawSprite sprite dest (x, y) = blitSurface sprite Nothing dest (Just $ Rect (x*32) (y*32) 32 32 )
+
+hpString :: Player -> String
+hpString p = (show $ pHealth p) ++ " / " ++ (show $ pMaxHealth p)
+
+enemyHpString :: Enemy -> String
+enemyHpString e = (show $ eHealth e) ++ "/" ++ (show $ eMaxHealth e)
 
 gameLoop :: GameState -> IO ()
 gameLoop gs = do
 
   s <- getVideoSurface
-
   gs' <- tickGame gs
-  SDL.flip s
 
-  let blit = (\(a, b) -> blitSurface (image gs) (Just $ Rect 70 70 32 32) s (Just (Rect a b 0 0)))
-  mapM_ blit (clickpos gs)
-
+  --reset screen
   fillRect s (Just $ Rect 0 0 800 608) (Pixel 0)
   
   let explored_pf = filter (\(x, y, _) -> (x,y,1) `elem` (fov gs)) (pf gs)
+
   let fillVal = (\(x, y, val) -> fillRect s (Just $ Rect (x*32) (y*32) 32 32) (Pixel (100*(1+(fromIntegral val)))))
+
   mapM_ fillVal explored_pf
+
+  drawSprite (playerSurface $ graphics gs) s (pPos $ gPlayer gs)
+
+  let enemySprite = enemySurface $ graphics gs
+  mapM_ (\e -> drawSprite enemySprite s (ePos e)) (enemies gs)
+
+  title <- renderTextSolid (font gs) (hpString $ gPlayer gs) (Color 255 0 0)
+  blitSurface title Nothing s (Just $ Rect 500 500 0 0)
+
+  let enemyToShow = (enemies gs) !! (gEnemyMouse gs)
+  strToBlit <- renderTextSolid (font gs) (enemyHpString $ enemyToShow) (Color 255 0 0)
+  blitSurface strToBlit Nothing s (Just $ Rect 500 400 0 0)
+
+  SDL.flip s
 
   case running gs of
     True -> gameLoop gs'
@@ -67,17 +78,32 @@ tickGame gs = do
   let ret = foldl handleEvent gs events
   return ret
 
+movePlayer :: Player -> Coord -> Player
+movePlayer p newPos = p {pPos = newPos}
+
+valueOf ::  (Int, Int) -> [(Int, Int, a)] -> (Int, Int, a)
+valueOf (mx, my) list = head $ filter (\(x',y',_) -> x' == mx && y' == my) list
+
+findEnemy :: Coord -> [Enemy] -> Int
+--findEnemy _ [] = -1
+findEnemy _ _ = 0
+
+handleAttack :: Coord -> GameState -> GameState
+handleAttack pos gs = gs {gEnemyMouse = findEnemy pos (enemies gs)}
+
 --Explore map
 handleClick :: GameState -> Coord -> GameState
 handleClick gs (mx, my)
   | notFree = trace "NOT" gs -- not explored yet, nothing happens
-  | hasEnemy = trace "Enemy" gs
-  | otherwise = gs {fov = fov'}
+  | hasEnemy = trace "Enemy" (handleAttack (mx, my) gs)
+  | hasWall = trace "Wall" gs
+  | otherwise = gs {fov = fov', gPlayer = movePlayer (gPlayer gs) (mx, my)}
   where notFree = (mx, my, 0) `elem` (fov gs)
-        hasEnemy = length (filter (\(x,y,_) -> x == mx && y == my) (enemies gs)) > 0
+        hasEnemy = length (filter (\e -> (snd $ ePos e) == mx && (fst $ ePos e) == my) (enemies gs)) > 0
+        hasWall = pfv /= 0
         fov' = map (u (mx, my)) (fov gs)
-        (_,_,zz) = head $ filter (\(x',y',_) -> x' == mx && y' == my) (fov gs)
-        (_,_,pfv) = head $ filter (\(x,y,_) -> x == mx && y == my) (pf gs)
+        (_,_,zz) = valueOf (mx, my) (fov gs) 
+        (_,_,pfv) = valueOf (mx, my) (pf gs)
         u = (\(x, y) (x', y', val) -> 
           if zz == 1 && 
               abs (x' - x) <= 1 && 
@@ -86,15 +112,18 @@ handleClick gs (mx, my)
             then (x', y', 1) 
             else (x', y', val))
 
+handleMouseOver :: GameState -> (Int, Int) -> GameState
+handleMouseOver gs (x, y) = gs
+
 handleEvent :: GameState -> Event -> GameState
 handleEvent gs e =
   case e of
     KeyDown (Keysym SDLK_SPACE _ _) -> gs {running = False}
     MouseButtonDown x y ButtonLeft  -> 
       handleClick gs (((fromIntegral x) `quot` 32), ((fromIntegral y) `quot` 32)) 
+    MouseMotion x y _ _ -> handleMouseOver gs (fromIntegral x, fromIntegral y)
     _                               -> gs
     
--- stolen code from mr cadr, works nice
 getEvents :: IO Event -> [Event] -> IO [Event]
 getEvents pEvent es = do
   e <- pEvent
